79b777032c7fe02604b84f3bfc26eca3a7ccce7d
TovRudyy <oleksandr.rudyy@gmail.com>
2018-10-06 20:14:46 +0200

    Classe Pla d'Estudis

---
+++ src/domain/PlaEstudis.java
@@ -0,0 +1,52 @@
+package domain;
+
+import java.util.ArrayList;
+
+/**
+ * @author  Oleksandr Rudyy,
+ * Date: 06/10/2018
+ */
+
+public class PlaEstudis {
+    String id;  //Acrònim del Pla d'Estudis
+    ArrayList<assignatura> assignatures = new ArrayList<assignatura>(); //Assignatures pertenyents al pla d'estudis
+
+    public PlaEstudis(String id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @param assig
+     * @return true si l'element s'ha afegit a les assignatures del Pla d'Estudis, false si no.
+     */
+    public boolean addAssignatura(assignatura assig) {
+        return assignatures.add(assig);
+    }
+
+    /**
+     *
+     * @return el id del Pla d'Estudis
+     */
+    public String getID() {
+        return this.id;
+    }
+
+    /**
+     *
+     * @return un element String[] amb els identificadors de totes les assignatures del pla d'estudis
+     */
+    public String[] getAssignatures(){
+        String[] id_assignatures;
+        int i;
+
+        id_assignatures = new String[assignatures.size()];
+        i = 0;
+        for (assignatura  a : assignatures) {
+            id_assignatures[i] = a.getId();
+            i++;
+        }
+        return id_assignatures;
+    }
+
+}
---
+++ src/tests_main.java
@@ -0,0 +1,18 @@
+import domain.PlaEstudis;
+import java.util.Scanner;
+
+public class tests_main {
+    public static void main(String [] args) {
+        Scanner keyboard;
+        String input, id;
+        PlaEstudis pe;
+
+        keyboard = new Scanner(System.in);
+        System.out.println("Introdueix un acrònim de pla d'estudis");
+        input = keyboard.nextLine();
+        pe = new PlaEstudis(input);
+
+        id = pe.getID();
+        System.out.print("Identificador del Pla d'Estudis: " + id);
+    }
+}
