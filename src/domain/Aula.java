0d275eef4bfdf69c008cbb3279b4f13d5d0d2d2b
DESKTOP-LSNRL66\victo <victordivi98@gmail.com>
2018-10-09 12:23:00 +0200

    Ja es pot alliberar les aules

--- src/domain/Aula.java
+++ src/domain/Aula.java
@@ -100,13 +100,25 @@ public class Aula {
     }
 
     /**
-     * Reserva l'Aula durant unes hores consecutives
-     * @param dia Dia de la setmana de la reserva
-     * @param hora Hora del dia de la reserva
-     * @param durant Nombre d'hores de la reserva
-     * @return @true si s'ha pogut fer la reserva, @false si alguna de les hores ja estava reservada
+     * Reserva l'Aula durant totes les hores de la setmana
      */
-    public boolean reserva(int dia, int hora, int durant) throws Exception{
+    public void reservar(){
+        for(boolean[] bb : ocupacio){
+            for(boolean b : bb){
+                b = true;
+            }
+        }
+    }
+
+    /**
+     * Reserva l'Aula durant unes hores consecutives
+     * @param dia Dia de la setmana de la reservar (0 <= dia <= 4)
+     * @param hora Hora del dia de la reservar (8 <= hora <= 19)
+     * @param durant Nombre d'hores de la reservar (hora + durant < 20)
+     * @return @true si s'ha pogut fer la reservar, @false si alguna de les hores ja estava reservada
+     * @throws Exception Algun dels parametres passats no es valid
+     */
+    public boolean reservar(int dia, int hora, int durant) throws Exception{
         if(dia < 0 || dia > 4 || hora < 8 || hora > 19 || durant > 12 || (durant + hora)>19) throw new Exception();
         for(int i=0; i<durant; ++i){
             if(ocupacio[hora+i-8][dia]) return false;
@@ -115,6 +127,32 @@ public class Aula {
         }
         return true;
     }
+
+    /**
+     * Elimina totes les reserves de l'Aula
+     */
+    public void alliberar(){
+        for(boolean[] bb : ocupacio){
+            for(boolean b : bb){
+                b = false;
+            }
+        }
+    }
+
+    /**
+     * Anula qualsevol reservar de l'Aula en unes hores consecutives (si l'Aula no estava reservada no fa res)
+     * @param dia Dia de la setmana de la reservar a anular (0 <= dia <= 4)
+     * @param hora Hora del dia de la reservar a anular (8 <= hora <= 19)
+     * @param durant Nombre d'hores que dura la reservar a anular (hora + durant < 20)
+     * @throws Exception Algun dels parametres no es valid
+     */
+    public void alliberar(int dia, int hora, int durant) throws Exception{
+        if(dia < 0 || dia > 4 || hora < 8 || hora > 19 || durant > 12 || (durant + hora)>19) throw new Exception();
+        for(int i=0; i<durant; ++i){
+            ocupacio[hora+i-8][dia] = false;
+        }
+    }
+
 
     /**
      * @return String amb les dades de l'Aula
--- src/testsClasses/test_aula.java
+++ src/testsClasses/test_aula.java
@@ -9,9 +9,9 @@ public class test_aula {
     public static void main(String[] args) throws Exception {
         Aula aula = new Aula("A5001", 30, Tipus_Aula.TEORIA);
         System.out.println(aula.toString());
-        aula.reserva(2, 8, 2);
-        aula.reserva(0, 14, 3);
-        aula.reserva(4, 10, 4);
+        aula.reservar(2, 8, 2);
+        aula.reservar(0, 14, 3);
+        aula.reservar(4, 10, 4);
         System.out.println(Arrays.deepToString(aula.getOcupacio()));
         System.out.println();
         System.out.println(aula.ocupacioToString());
