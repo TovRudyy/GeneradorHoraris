7366efe1acd795fa6c3b96101264151e32e2fa2e
TovRudyy <oleksandr.rudyy@gmail.com>
2018-10-21 20:18:23 +0200

    S'ha afegit a GrupConcret un mètodes per emmagatzemar totes les possibles Classes + test del generador de classes

--- src/domain/GrupConcret.java
+++ src/domain/GrupConcret.java
@@ -2,6 +2,11 @@ package domain;
 
 // Aquesta classe ens permet tenir tota la informacio necessaria per a calcular l'horari d'un subgrup concret
 
+import persistencia.Lector_Aules;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * @Author: David
  */
@@ -13,6 +18,11 @@ public class GrupConcret {
     private int capacitat, nivellAssig;
     private Tipus_Aula tAula;
     private Tipus_Lab tLab = null;
+
+    private int inici_possible;
+    private int final_possible;
+    private ArrayList<Classe> possibles_classes;
+    private ArrayList<Classe> classes_assignades;
 
     //numero i duració de classes
     private int classes_teoria, classes_problemes, classes_laboratori;
@@ -26,6 +36,9 @@ public class GrupConcret {
         this.tAula = tAula;
         this.idAssig = idAssig;
         this.nivellAssig = nivellAssig;
+        this.inici_possible = 8;
+        this.final_possible = 20;
+        this.possibles_classes = generaPossiblesClasses();
     }
 
     public GrupConcret (String idGrup, int cap, Tipus_Aula tAula, Tipus_Lab tLab, String idAssig, int nivellAssig) {
@@ -35,6 +48,9 @@ public class GrupConcret {
         this.idAssig = idAssig;
         this.nivellAssig = nivellAssig;
         this.tLab = tLab;
+        this.inici_possible = 8;
+        this.final_possible = 20;
+        this.possibles_classes = generaPossiblesClasses();
     }
 
 
@@ -55,6 +71,29 @@ public class GrupConcret {
     public void showAll () {
         if (tLab == null) System.out.println (idGrup + ":" + capacitat + ":"+ tAula + ":" + idAssig +":"+ nivellAssig);
         else System.out.println (idGrup + ":" + capacitat + ":"+ tAula + ":" + tLab +":"+ idAssig +":"+ nivellAssig);
+    }
+
+    private ArrayList<Classe> generaPossiblesClasses() {
+        Map<String, Aula> aules = Lector_Aules.getAules();
+        ArrayList<Classe> totesClasses = new ArrayList<>();
+        for (Aula aula : aules.values()) {
+            for (DiaSetmana dia : DiaSetmana.values()) {
+                for (int i = inici_possible; i < final_possible; i++) {
+                    Classe aux = new Classe(dia, i, aula);
+                    totesClasses.add(aux);
+                }
+            }
+        }
+        return totesClasses;
+    }
+
+    //Funció trivial per testos ~Olek
+    public void printPossiblesClasses() {
+        for (Classe classe: possibles_classes) {
+            System.out.println(classe.getAula().getId());
+            System.out.println(classe.getDia());
+            System.out.println(classe.getHoraInici());
+        }
     }
 
 
---
+++ src/testsClasses/testGrupConcret.java
@@ -0,0 +1,15 @@
+package testsClasses;
+
+import domain.GrupConcret;
+import domain.Tipus_Aula;
+import domain.grup;
+import persistencia.Lector_Aules;
+import persistencia.Lector_Pla;
+
+public class testGrupConcret {
+    public static void main(String [] argv) throws Exception {
+        Lector_Aules.readFolderAules();
+        GrupConcret b = new GrupConcret("10", 20, Tipus_Aula.TEORIA, "M1", 1);
+        b.printPossiblesClasses();
+    }
+}
