81cee2d7f9cbbc447dd64942cac7f3289b6c9c8d
TovRudyy <oleksandr.rudyy@gmail.com>
2018-10-22 19:03:34 +0200

    Canvi de nom testGrupConcret a testAssignacio

--- src/testsClasses/testAssignacio.java
+++ src/testsClasses/testAssignacio.java
@@ -4,7 +4,7 @@ import domain.Assignacio;
 import domain.Tipus_Aula;
 import persistencia.Lector_Aules;
 
-public class testGrupConcret {
+public class testAssignacio {
     public static void main(String [] argv) throws Exception {
         Lector_Aules.readFolderAules();
         Assignacio b = new Assignacio("10", 20, Tipus_Aula.TEORIA, "M1", 1);
