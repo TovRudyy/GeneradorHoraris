1e18c9d327871c8e1295f26a043f61d4edba9433
DESKTOP-LSNRL66\victo <victordivi98@gmail.com>
2018-11-01 10:52:32 +0100

    Eliminat Ocupacio de l'Aula

--- src/domain/Aula.java
+++ src/domain/Aula.java
@@ -15,7 +15,6 @@ public class Aula {
     private String id;              //Identificador de l'Aula
     private int capacitat;          //Capacitat de l'Aula
     private Tipus_Aula tipus;       //Tipus d'Aula
-    private boolean[][] ocupacio;   //Ocupacio de l'Aula (hores x dia)
 
 
     /**
@@ -27,7 +26,6 @@ public class Aula {
         this.id = id;
         this.capacitat = capacitat;
         this.tipus = tipus;
-        this.ocupacio = new boolean[12][5];
     }
 
     /**
@@ -65,98 +63,12 @@ public class Aula {
         return tipus;
     }
 
-
-
-
-
-
     /**
      * @param tipus Nou tipus de l'Aula
      */
     public void setTipus(Tipus_Aula tipus){
         this.tipus = tipus;
     }
-
-
-    /**
-     * @return Retorna l'Ocupació de l'Aula en forma de matriu de Booleans (true == ocupat)
-     */
-    public boolean[][] getOcupacio() {
-        return ocupacio;
-    }
-
-
-    /**
-     * @return Retorna l'Ocupació de l'Aula en forma d'horari (String) preparat per imprimir
-     */
-    public String ocupacioToString(){
-        StringBuilder s = new StringBuilder("               Dilluns  Dimarts  Dimecres Dijous   Divendres\n");
-        for(int i=0; i<12; ++i){
-            s.append(i+8).append(":00 - ").append(i+9).append(":00: ");
-            if(i==0) s.append("  ");
-            if(i==1) s.append(" ");
-            for(int j=0; j<5; ++j){
-                s.append((ocupacio[i][j]) ? "ocupat   " : "lliure   ");
-            }
-            s.append("\n");
-        }
-        return s.toString();
-    }
-
-    /**
-     * Reserva l'Aula durant totes les hores de la setmana
-     */
-    public void reservar(){
-        for ( boolean[] bb : ocupacio){
-            for(boolean b : bb){
-                b = true;
-            }
-        }
-    }
-
-    /**
-     * Reserva l'Aula durant unes hores consecutives
-     * @param dia Dia de la setmana de la reservar (0 <= dia <= 4)
-     * @param hora Hora del dia de la reservar (8 <= hora <= 19)
-     * @param durant Nombre d'hores de la reservar (hora + durant < 20)
-     * @return @true si s'ha pogut fer la reservar, @false si alguna de les hores ja estava reservada
-     * @throws Exception Algun dels parametres passats no es valid
-     */
-    public boolean reservar(int dia, int hora, int durant) throws Exception{
-        if(dia < 0 || dia > 4 || hora < 8 || hora > 19 || durant > 12 || (durant + hora)>19) throw new Exception();
-        for(int i=0; i<durant; ++i){
-            if(ocupacio[hora+i-8][dia]) return false;
-        }for(int i=0; i<durant; ++i){
-            ocupacio[hora+i-8][dia] = true;
-        }
-        return true;
-    }
-
-    /**
-     * Elimina totes les reserves de l'Aula
-     */
-    public void alliberar(){
-        for(boolean[] bb : ocupacio){
-            for(boolean b : bb){
-                b = false;
-            }
-        }
-    }
-
-    /**
-     * Anula qualsevol reservar de l'Aula en unes hores consecutives (si l'Aula no estava reservada no fa res)
-     * @param dia Dia de la setmana de la reservar a anular (0 <= dia <= 4)
-     * @param hora Hora del dia de la reservar a anular (8 <= hora <= 19)
-     * @param durant Nombre d'hores que dura la reservar a anular (hora + durant < 20)
-     * @throws Exception Algun dels parametres no es valid
-     */
-    public void alliberar(int dia, int hora, int durant) throws Exception{
-        if(dia < 0 || dia > 4 || hora < 8 || hora > 19 || durant > 12 || (durant + hora)>19) throw new Exception();
-        for(int i=0; i<durant; ++i){
-            ocupacio[hora+i-8][dia] = false;
-        }
-    }
-
 
     /**
      * @return String amb les dades de l'Aula
--- src/testsClasses/test_aula.java
+++ src/testsClasses/test_aula.java
@@ -4,16 +4,8 @@ import domain.Aula;
 import domain.Tipus_Aula;
 
 public class test_aula {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Aula aula = new Aula("A5001", 30, Tipus_Aula.TEORIA);
         System.out.println(aula.toString());
-        aula.reservar(2, 8, 2);
-        aula.reservar(0, 14, 3);
-        aula.reservar(4, 10, 4);
-        System.out.println(aula.ocupacioToString());
-        aula.alliberar();
-        System.out.println(aula.ocupacioToString());
-        aula.reservar();
-        System.out.println(aula.ocupacioToString());
     }
 }
