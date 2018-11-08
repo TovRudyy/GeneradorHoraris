a7e03a89034cf770551f124075ed29c09d5a0680
DESKTOP-LSNRL66\victo <victordivi98@gmail.com>
2018-11-08 19:23:59 +0100

    Afegida funcio de llegir Array d'aules de un fitxer

--- src/persistencia/Lector_Pla_JSON.java
+++ src/persistencia/Lector_Pla_JSON.java
@@ -18,79 +18,82 @@ public final class Lector_Pla_JSON {
     private Lector_Pla_JSON() {
     }
 
-    private static void afegeixAssignatures(PlaEstudis plaEstudis, JSONArray assignatures) throws Aula_Exception{
-        for(Object a: assignatures){
-            JSONObject ass = (JSONObject) a;
-            String codi = (String) ass.get("codi");
-            String nom = (String) ass.get("nom");
-            int nivell = ((Long) ass.get("nivell")).intValue();
+    private static assignatura creaAssignatura(JSONObject ass) throws Aula_Exception{
+        String codi = (String) ass.get("codi");
+        String nom = (String) ass.get("nom");
+        int nivell = ((Long) ass.get("nivell")).intValue();
 
-            assignatura assig = new assignatura(codi, nom, nivell);
+        assignatura assig = new assignatura(codi, nom, nivell);
 
-            JSONObject classes = (JSONObject) ass.get("classes");
-            int nTeoria = ((Long) classes.get("teoria")).intValue();
-            int dTeoria = ((Long) classes.get("duracio_t")).intValue();
-            int nProblemes = ((Long) classes.get("problemes")).intValue();
-            int dProblemes = ((Long) classes.get("duracio_p")).intValue();
-            int nLaboratori = ((Long) classes.get("laboratori")).intValue();
-            int dLaboratori = ((Long) classes.get("duracio_l")).intValue();
-            assig.setClasses(nTeoria, dTeoria, nLaboratori, dLaboratori, nProblemes, dProblemes);
+        JSONObject classes = (JSONObject) ass.get("classes");
+        int nTeoria = ((Long) classes.get("teoria")).intValue();
+        int dTeoria = ((Long) classes.get("duracio_t")).intValue();
+        int nProblemes = ((Long) classes.get("problemes")).intValue();
+        int dProblemes = ((Long) classes.get("duracio_p")).intValue();
+        int nLaboratori = ((Long) classes.get("laboratori")).intValue();
+        int dLaboratori = ((Long) classes.get("duracio_l")).intValue();
+        assig.setClasses(nTeoria, dTeoria, nLaboratori, dLaboratori, nProblemes, dProblemes);
 
-            JSONObject grups = (JSONObject) ass.get("grups");
+        JSONObject grups = (JSONObject) ass.get("grups");
 
-            String tipus = (String) grups.get("tipus_subgrup");
-            ArrayList<Tipus_Aula> tipus_aules = new ArrayList<>();
-            for(String t:tipus.split(";")){
-                tipus_aules.add(Tipus_Aula.string_to_Tipus_Aula(t));
+        String tipus = (String) grups.get("tipus_subgrup");
+        ArrayList<Tipus_Aula> tipus_aules = new ArrayList<>();
+        for(String t:tipus.split(";")){
+            tipus_aules.add(Tipus_Aula.string_to_Tipus_Aula(t));
+        }
+        TreeMap<String, grup> grupMap = new TreeMap<>();
+        if((boolean) grups.get("automatic")){
+            int n_grups = ((Long) grups.get("nombre")).intValue();
+            int capacitat = ((Long) grups.get("capacitat")).intValue();
+            int n_matins = ((Long) grups.get("matins")).intValue();
+
+            for(int i=1; i<=n_grups; ++i) grupMap.put(String.valueOf(i*10), new grup(String.valueOf(i*10), capacitat, (i<=n_matins)? "M" : "T", Tipus_Aula.TEORIA));
+
+            if((boolean) grups.get("subgrup_per_alumnes")) {
+                int alumnes_subgrup = ((Long) grups.get("valor")).intValue();
+                for (int i = 1; i <= n_grups; i++) {
+                    for (int j = 1; j * alumnes_subgrup <= capacitat; ++j) {
+                        for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(i*10 + j), new grup(String.valueOf(i*10+j), alumnes_subgrup, (i<=n_matins)? "M" : "T", t));
+                    }
+                }
+            }else{
+                int n_subgrups = ((Long) grups.get("valor")).intValue();
+                for (int i = 1; i <= n_grups; i++) {
+                    for (int j = 1; j <= n_subgrups; ++j) {
+                        for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(i*10 + j), new grup(String.valueOf(i*10+j), capacitat/n_subgrups, (i<=n_matins)? "M" : "T", t));
+                    }
+                }
             }
-            TreeMap<String, grup> grupMap = new TreeMap<>();
-            if((boolean) grups.get("automatic")){
-                int n_grups = ((Long) grups.get("nombre")).intValue();
-                int capacitat = ((Long) grups.get("capacitat")).intValue();
-                int n_matins = ((Long) grups.get("matins")).intValue();
-
-                for(int i=1; i<=n_grups; ++i) grupMap.put(String.valueOf(i*10), new grup(String.valueOf(i*10), capacitat, (i<=n_matins)? "M" : "T", Tipus_Aula.TEORIA));
-
-                if((boolean) grups.get("subgrup_per_alumnes")) {
-                    int alumnes_subgrup = ((Long) grups.get("valor")).intValue();
-                    for (int i = 1; i <= n_grups; i++) {
-                        for (int j = 1; j * alumnes_subgrup <= capacitat; ++j) {
-                            for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(i*10 + j), new grup(String.valueOf(i*10+j), alumnes_subgrup, (i<=n_matins)? "M" : "T", t));
-                        }
+        }
+        else{
+            JSONArray gs = (JSONArray) grups.get("grups");
+            for(Object o:gs){
+                JSONObject g = (JSONObject) o;
+                int nom_grup = ((Long) g.get("nom")).intValue();
+                if(nom_grup % 10 != 0 ) throw new Aula_Exception("Nom de grup no valid. Assignatura: " + nom);
+                String horari = (String) g.get("horari");
+                int capacitat = ((Long) g.get("capacitat")).intValue();
+                grupMap.put(String.valueOf(nom_grup), new grup(String.valueOf(nom_grup), capacitat, horari, Tipus_Aula.TEORIA));
+                if((boolean) g.get("subgrup_per_alumnes")) {
+                    int alumnes_subgrup = ((Long) g.get("valor")).intValue();
+                    for (int j = 1; j * alumnes_subgrup <= capacitat; ++j) {
+                        for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(nom_grup + j), new grup(String.valueOf(nom_grup+j), alumnes_subgrup, horari, t));
                     }
                 }else{
-                    int n_subgrups = ((Long) grups.get("valor")).intValue();
-                    for (int i = 1; i <= n_grups; i++) {
-                        for (int j = 1; j <= n_subgrups; ++j) {
-                            for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(i*10 + j), new grup(String.valueOf(i*10+j), capacitat/n_subgrups, (i<=n_matins)? "M" : "T", t));
-                        }
+                    int n_subgrups = ((Long) g.get("valor")).intValue();
+                    for (int j = 1; j <= n_subgrups; ++j) {
+                        for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(nom_grup + j), new grup(String.valueOf(nom_grup+j), capacitat/n_subgrups, horari, t));
                     }
                 }
             }
-            else{
-                JSONArray gs = (JSONArray) grups.get("grups");
-                for(Object o:gs){
-                    JSONObject g = (JSONObject) o;
-                    int nom_grup = ((Long) g.get("nom")).intValue();
-                    if(nom_grup % 10 != 0 ) throw new Aula_Exception("Nom de grup no valid. Pla:" + plaEstudis.getID() +  ". Assignatura: " + nom);
-                    String horari = (String) g.get("horari");
-                    int capacitat = ((Long) g.get("capacitat")).intValue();
-                    grupMap.put(String.valueOf(nom_grup), new grup(String.valueOf(nom_grup), capacitat, horari, Tipus_Aula.TEORIA));
-                    if((boolean) g.get("subgrup_per_alumnes")) {
-                        int alumnes_subgrup = ((Long) g.get("valor")).intValue();
-                        for (int j = 1; j * alumnes_subgrup <= capacitat; ++j) {
-                            for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(nom_grup + j), new grup(String.valueOf(nom_grup+j), alumnes_subgrup, horari, t));
-                        }
-                    }else{
-                        int n_subgrups = ((Long) g.get("valor")).intValue();
-                        for (int j = 1; j <= n_subgrups; ++j) {
-                            for (Tipus_Aula t:tipus_aules) grupMap.put(String.valueOf(nom_grup + j), new grup(String.valueOf(nom_grup+j), capacitat/n_subgrups, horari, t));
-                        }
-                    }
-                }
-            }
-            assig.creaGrups(grupMap);
-            plaEstudis.addAssignatura(assig);
+        }
+        assig.creaGrups(grupMap);
+        return assig;
+    }
+
+    private static void afegeixAssignatures(PlaEstudis plaEstudis, JSONArray assignatures) throws Aula_Exception{
+        for(Object a: assignatures){
+            plaEstudis.addAssignatura(creaAssignatura((JSONObject) a));
         }
     }
 
@@ -135,8 +138,13 @@ public final class Lector_Pla_JSON {
         return plansEstudis;
     }
 
-    public static assignatura llegeixAssignatura(String path) {
-        return null;
+    public static ArrayList<assignatura> llegeixAssignatura(String path) throws Aula_Exception, IOException, ParseException{
+        JSONParser parser = new JSONParser();
+        JSONArray assigs = (JSONArray) parser.parse(new FileReader(path));
+        ArrayList<assignatura> assignatures = new ArrayList<>();
+        for(Object a : assigs) assignatures.add(creaAssignatura((JSONObject) a));
+        return  assignatures;
+
     }
 
 }
