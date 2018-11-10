749575e107840e2cc864bf3887158c351c6e0dc7
Victor-PC\Victor <victordivi98@gmail.com>
2018-11-10 14:38:28 +0100

    Afegit algun comentari aclaratori. Ara es torna a escriure el menu principal quan tornes al Main des d'algun driver

--- src/testsClasses/Drivers/MainDriver.java
+++ src/testsClasses/Drivers/MainDriver.java
@@ -2,7 +2,12 @@ package testsClasses.Drivers;
 
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class MainDriver {
+
+    static Scanner keyboard;
+
     public static void main(String[] args) {
         printMenu();
         executar();
@@ -10,72 +15,89 @@ public class MainDriver {
 
     private static void executar(){
         int codi;
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Introdueix un codi: ");
+        keyboard = new Scanner(System.in);
+        out.println("Introdueix un codi: ");
         while((codi = keyboard.nextInt()) != 12){
             switch (codi) {
+                case -1:
+                    printCodis();
+                    break;
                 case 1:
                     driverTipus_Aula.main();
+                    printMenu();
                     break;
                 case 2:
                     driverAula.main();
+                    printMenu();
                     break;
                 case 3:
                     driverClasse.main();
+                    printMenu();
                     break;
                 case 4:
                     driverGrup.main();
+                    printMenu();
                     break;
                 case 5:
                     driverRestriccioOcupacio.main();
+                    printMenu();
                     break;
                 case 6:
                     driverRestriccioSubgrup.main();
+                    printMenu();
                     break;
                 case 7:
                     driverControladorAules.main();
+                    printMenu();
                     break;
                 case 8:
                     driverAssignacio.main();
+                    printMenu();
                     break;
                 case 9:
                     driverAssignatura.main();
+                    printMenu();
                     break;
                 case 10:
                     driverHorari.main();
+                    printMenu();
                     break;
                 case 11:
                     driverPlaEstudis.main();
+                    printMenu();
                     break;
                 default:
-                    System.out.println("Codi no valid.");
+                    out.println("Codi no valid. Aqui tens els codis que ho son:");
+                    printCodis();
             }
-            System.out.println();
-            System.out.println("Introdueix un codi: ");
+            out.println();
+            out.println("Introdueix un codi: ");
         }
     }
 
     private static void printMenu() {
-        System.out.println("Benvingut al menu principal dels drivers.");
-        System.out.println("Selecciona el driver que vols utilitzar introduint el seu codi associat:");
+        out.println("Benvingut al menu principal dels drivers.");
+        out.println("Selecciona el driver que vols utilitzar introduint el seu codi associat:");
         printCodis();
-        System.out.println("Els drivers estan ordenats segons l'us que fan les classes de les altres, de forma que una classe pot utilitzar una altre amb codi inferior, pero no amb codi superior.");
-        System.out.println("Per exemple, la classe Aula conte un atribut Tipus_Aula, pero cap de les altres classes, mentre que Pla d'Estudis conte, entre altres, Horari i Assignatura, pero ninguna classe de dades el conte a ell.");
-        System.out.println("L'unica excepcio son les classes Grup i Restriccio_Subgrup, que es contenen la una a la altra, de manera que en el driver de Grup s'utilitza un Stub de Restriccio_Subgrup.");
+        out.println("Els drivers estan ordenats segons l'us que fan les classes de les altres, de forma que una classe pot utilitzar una altre amb codi inferior, pero no amb codi superior.");
+        out.println("Per exemple, la classe Aula conte un atribut Tipus_Aula, pero no conte cap de les altres classes, mentre que Pla d'Estudis conte, entre altres, Horari i Assignatura, pero ninguna classe de dades el conte a ell.");
+        out.println("L'unica excepcio son les classes Grup i Restriccio_Subgrup, que es contenen la una a la altra, de manera que en el driver de Grup s'utilitza un Stub de Restriccio_Subgrup.");
+        out.println("Recorda que les execucions dels drivers son independents. Es a dir, qualsevol cosa que facis en un driver no es conservara quan hi surtis.");
+        out.println("Si vols tronar a consultar els codis, pots introduir el codi -1 (tambe valid en tots els drivers).");
     }
 
     private static void printCodis(){
-        System.out.println("\t1) Tipus Aula");
-        System.out.println("\t2) Aula");
-        System.out.println("\t3) Classe");
-        System.out.println("\t4) Grup");
-        System.out.println("\t5) Restriccio Ocupacio");
-        System.out.println("\t6) Restriccio Subgrup");
-        System.out.println("\t7) Corequisit");
-        System.out.println("\t8) Assignacio");
-        System.out.println("\t9) Assignatura");
-        System.out.println("\t10) Horari");
-        System.out.println("\t11) Pla d'Estudis");
-        System.out.println("\t12) Sortir");
+        out.println("\t1) Tipus Aula");
+        out.println("\t2) Aula");
+        out.println("\t3) Classe");
+        out.println("\t4) Grup");
+        out.println("\t5) Restriccio Ocupacio");
+        out.println("\t6) Restriccio Subgrup");
+        out.println("\t7) Corequisit");
+        out.println("\t8) Assignacio");
+        out.println("\t9) Assignatura");
+        out.println("\t10) Horari");
+        out.println("\t11) Pla d'Estudis");
+        out.println("\t12) Sortir");
     }
 }
