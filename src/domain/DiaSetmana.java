5db6d33a7294c9134c1a852b0de531adab1238a8
Victor-PC\Victor <victordivi98@gmail.com>
2018-11-10 17:47:46 +0100

    Afegida funcio de String-> Dia per utilitzar en els drivers

--- src/domain/DiaSetmana.java
+++ src/domain/DiaSetmana.java
@@ -2,4 +2,14 @@ package domain;
 
 public enum DiaSetmana {
     DILLUNS, DIMARTS, DIMECRES, DIJOUS, DIVENDRES;
+
+    public static DiaSetmana string_To_DiaSetmana(String s){
+        s = s.toUpperCase();
+        if(s.equals("DL") || s.equals("DILLUNS")) return DILLUNS;
+        if(s.equals("DM") || s.equals("DIMARTS")) return DILLUNS;
+        if(s.equals("DC") || s.equals("DIMECRES")) return DILLUNS;
+        if(s.equals("DJ") || s.equals("DIJOUS")) return DILLUNS;
+        if(s.equals("DV") || s.equals("DIVENDRES")) return DILLUNS;
+        throw new IllegalArgumentException();
+    }
 }
