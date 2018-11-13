b4bb7b052d1dcc8a78bc5263e0782f5a2c6fabb3
Victor-PC\Victor <victordivi98@gmail.com>
2018-11-14 00:16:54 +0100

    Afegides dos alteracions de printHorari: - Per Aula - Per Assignatura

--- src/domain/Horari.java
+++ src/domain/Horari.java
@@ -204,6 +204,136 @@ public class Horari {
         }
     }
 
+    public void printHorariAules(){
+        Set<String> aules = new TreeSet<>();
+        for(assignacio a: conjuntAssignacions.values()) for (Classe c : a.getSeleccionades()) aules.add(c.getIdAula());
+        for(String aula: aules){
+            System.out.println("Aula " + aula);
+            String formatHeader = "|%-15s|%-20s|%-20s|%-20s|%-20s|%-20s|\n";
+            System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+            System.out.format(formatHeader, "   Hora/Dia", "      DILLUNS", "      DIMARTS", "     DIMECRES", "      DIJOUS", "     DIVENDRES");
+            System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+
+            ArrayList<ArrayList<Queue<String>>> horaris = new ArrayList<ArrayList<Queue<String>>>();
+            for(int i=0; i<12; ++i){
+                horaris.add(new ArrayList<>());
+                for(int j=0; j<5; ++j){
+                    horaris.get(i).add(new PriorityQueue<>());
+                }
+            }
+            for(assignacio a:conjuntAssignacions.values()){
+                for (Classe c: a.getSeleccionades()) {
+                    if(!c.getIdAula().equals(aula)) continue;
+                    int dia = 0;
+                    switch (c.getDia()) {
+                        case DILLUNS:
+                            dia = 0;
+                            break;
+                        case DIMARTS:
+                            dia = 1;
+                            break;
+                        case DIMECRES:
+                            dia = 2;
+                            break;
+                        case DIJOUS:
+                            dia = 3;
+                            break;
+                        case DIVENDRES:
+                            dia = 4;
+                    }
+                    int ini = c.getHoraInici(), fi = c.getHoraFi();
+                    for (int i = ini; i < fi; ++i) {
+                        horaris.get(i - 8).get(dia).add("     " + c.getId_assig() + "    " + c.getId_grup());
+                    }
+                }
+            }
+
+            int hora0 = 8, hora1=9;
+            for(ArrayList<Queue<String>> hora : horaris){
+                boolean fi = false;
+                boolean first = true;
+                while(!fi){
+                    String dilluns = (hora.get(0).isEmpty())? " " : hora.get(0).remove();
+                    String dimarts = (hora.get(1).isEmpty())? " " : hora.get(1).remove();
+                    String dimecres = (hora.get(2).isEmpty())? " " : hora.get(2).remove();
+                    String dijous = (hora.get(3).isEmpty())? " " : hora.get(3).remove();
+                    String divendres = (hora.get(4).isEmpty())? " " : hora.get(4).remove();
+                    System.out.format(formatHeader, (first)? " " + hora0 + ":00 - " + hora1 + ":00" : "", dilluns, dimarts, dimecres, dijous, divendres);
+                    first = false;
+                    fi = (hora.get(0).isEmpty() && hora.get(1).isEmpty() && hora.get(2).isEmpty() && hora.get(3).isEmpty() && hora.get(4).isEmpty());
+                }
+                ++hora0; ++hora1;
+                System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+                System.out.println();
+            }
+        }
+    }
+
+    public void printHorariAssignatures(){
+        Set<String> assigs = new TreeSet<>();
+        for(assignacio a: conjuntAssignacions.values()) assigs.add(a.getIdAssig());
+        for(String ass: assigs){
+            System.out.println("Assignatura " + ass);
+            String formatHeader = "|%-15s|%-20s|%-20s|%-20s|%-20s|%-20s|\n";
+            System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+            System.out.format(formatHeader, "   Hora/Dia", "      DILLUNS", "      DIMARTS", "     DIMECRES", "      DIJOUS", "     DIVENDRES");
+            System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+
+            ArrayList<ArrayList<Queue<String>>> horaris = new ArrayList<ArrayList<Queue<String>>>();
+            for(int i=0; i<12; ++i){
+                horaris.add(new ArrayList<>());
+                for(int j=0; j<5; ++j){
+                    horaris.get(i).add(new PriorityQueue<>());
+                }
+            }
+            for(assignacio a:conjuntAssignacions.values()){
+                for (Classe c: a.getSeleccionades()) {
+                    if(!c.getId_assig().equals(ass)) continue;
+                    int dia = 0;
+                    switch (c.getDia()) {
+                        case DILLUNS:
+                            dia = 0;
+                            break;
+                        case DIMARTS:
+                            dia = 1;
+                            break;
+                        case DIMECRES:
+                            dia = 2;
+                            break;
+                        case DIJOUS:
+                            dia = 3;
+                            break;
+                        case DIVENDRES:
+                            dia = 4;
+                    }
+                    int ini = c.getHoraInici(), fi = c.getHoraFi();
+                    for (int i = ini; i < fi; ++i) {
+                        horaris.get(i - 8).get(dia).add("     " + c.getId_grup() + "   " + c.getIdAula());
+                    }
+                }
+            }
+
+            int hora0 = 8, hora1=9;
+            for(ArrayList<Queue<String>> hora : horaris){
+                boolean fi = false;
+                boolean first = true;
+                while(!fi){
+                    String dilluns = (hora.get(0).isEmpty())? " " : hora.get(0).remove();
+                    String dimarts = (hora.get(1).isEmpty())? " " : hora.get(1).remove();
+                    String dimecres = (hora.get(2).isEmpty())? " " : hora.get(2).remove();
+                    String dijous = (hora.get(3).isEmpty())? " " : hora.get(3).remove();
+                    String divendres = (hora.get(4).isEmpty())? " " : hora.get(4).remove();
+                    System.out.format(formatHeader, (first)? " " + hora0 + ":00 - " + hora1 + ":00" : "", dilluns, dimarts, dimecres, dijous, divendres);
+                    first = false;
+                    fi = (hora.get(0).isEmpty() && hora.get(1).isEmpty() && hora.get(2).isEmpty() && hora.get(3).isEmpty() && hora.get(4).isEmpty());
+                }
+                ++hora0; ++hora1;
+                System.out.format("+---------------+--------------------+--------------------+--------------------+--------------------+--------------------+\n");
+                System.out.println();
+            }
+        }
+    }
+
     public String toString() {
         // Es crea un nou stream
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -212,7 +342,7 @@ public class Horari {
         PrintStream old = System.out;
         // S'indica a java que passi a utilitzar el nou stream
         System.setOut(ps);
-        printHorari();
+        printHorariAssignatures();
         // Es restaura el stream original:
         System.out.flush();
         System.setOut(old);
