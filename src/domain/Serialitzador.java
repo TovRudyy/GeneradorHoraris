cda28606aef0ab31bccb407b1e1ccb730f895092
DESKTOP-LSNRL66\victo <victordivi98@gmail.com>
2018-12-13 18:08:30 +0100

    Serialization

--- src/domain/Serialitzador.java
+++ src/domain/Serialitzador.java
@@ -7,39 +7,29 @@ public final class Serialitzador {
     private Serialitzador() {
     }
 
-    private static class Serializer<T> {
-
-        private void serialize(T object, String path) throws IOException{
-            FileOutputStream fileOut = new FileOutputStream(path);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(object);
-            out.close();
-            fileOut.close();
-        }
-
-        @SuppressWarnings("unchecked")
-        private T deserialize(String path) throws IOException, ClassNotFoundException{
-            FileInputStream fileIn = new FileInputStream(path);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            T object = (T) in.readObject();
-            in.close();
-            fileIn.close();
-            return object;
-        }
-    }
-
     public static <T> void guarda(T objecte, String path) throws IOException {
-        Serializer<T> serializer = new Serializer<>();
-        serializer.serialize(objecte, path);
+        serialize(objecte, path);
     }
 
     public static void guarda(String path, Object... objectes) throws IOException {
-        Serializer<Object[]> serializer = new Serializer<>();
-        serializer.serialize(objectes, path);
+        serialize(objectes, path);
     }
 
+    private static <T> void serialize(T object, String path) throws IOException{
+        FileOutputStream fileOut = new FileOutputStream(path);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(object);
+        out.close();
+        fileOut.close();
+    }
+
+    @SuppressWarnings("unchecked")
     public static <T> T carrega(String path) throws IOException, ClassNotFoundException{
-        Serializer<T> serializer = new Serializer<>();
-        return serializer.deserialize(path);
+        FileInputStream fileIn = new FileInputStream(path);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        T object = (T) in.readObject();
+        in.close();
+        fileIn.close();
+        return object;
     }
 }
--- src/testsClasses/TestMain.java
+++ src/testsClasses/TestMain.java
@@ -1,55 +1,44 @@
 package testsClasses;
 
-import domain.Aula;
-import persistencia.Lector_Aules_JSON;
-import persistencia.Serializer;
+import domain.*;
 
-import java.io.IOException;
-import java.util.Map;
 import java.util.Scanner;
 
 public class TestMain {
 
     public static void main(String[] args) {
-        System.out.println("Serialize: 1");
-        System.out.println("Deserialize: 2");
         Scanner keyboard = new Scanner(System.in);
-        String codi;
-        while(keyboard.hasNext()){
-            switch (keyboard.next()){
-                case "1":
-                    ser();
-                    break;
-                case "2":
-                    deser();
-                    break;
-                default:
-                    System.out.println("Error");
-            }
-            System.out.println("Serialize: 1");
-            System.out.println("Deserialize: 2");
+        switch (keyboard.next()){
+            case "1":
+                ser();
+                break;
+            case "2":
+                deser();
+                break;
         }
     }
 
     private static void ser(){
+        Aula aula = new Aula("Aula", 60, Tipus_Aula.TEORIA);
+        grup grup = new grup("Grup", 30, "M", Tipus_Aula.TEORIA);
+        Classe classe = new Classe("Assig", "10", DiaSetmana.DILLUNS, 8, 10, "Aula");
         try{
-            Map<String, Aula> aules = Lector_Aules_JSON.llegirCarpetaAules();
-            for(Aula aula: aules.values()) System.out.println(aula.toString());
-            Serializer<Map<String, Aula>> serializer = new Serializer<>();
-            serializer.serialize(aules, "data/Serialized/AulesMap.ser");
+            Serialitzador.guarda("data/Array.ser", aula, grup, classe);
+            Serialitzador.guarda("data/O.ser", aula);
         }catch(Exception e){
             e.printStackTrace();
         }
+
     }
 
     private static void deser(){
-        Serializer<Map<String, Aula>> serializer = new Serializer<>();
         try{
-            Map<String, Aula> aules = serializer.deserialize("data/Serialized/AulesMap.ser");
-            for(Aula aula: aules.values()) System.out.println(aula.toString());
-        }catch (IOException | ClassNotFoundException e){
-            System.out.println(e.getMessage());
+            Object obj = Serialitzador.carrega("data/O.ser");
+            Aula aula = (Aula) obj;
+            System.out.println(aula.toString());
+        }catch(Exception e){
             e.printStackTrace();
         }
+
     }
 }
