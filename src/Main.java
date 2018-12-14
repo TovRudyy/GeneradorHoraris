377d1361ab1a3052104b702c894e60ebb349edd1
David Pujol <david.pujolperich@gmail.com>
2018-12-14 11:07:30 +0100

    He afegit la restriccio de que no es pugui donar en un interval donat i a mes he fet que el horari tingui en compte les restriccions flexibles que li passa el pla d'estudis, que es qui la mante actualitzada.

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         //Per utilitzar la GUI:
-        //Application.launch(VistaPrincipal.class, args);
+        Application.launch(VistaPrincipal.class, args);
 
         //Per utilitzar la CLI:
-        CtrlMnPrincipal GH = new CtrlMnPrincipal();
+       /* CtrlMnPrincipal GH = new CtrlMnPrincipal();
         try {
             GH.runGeneradorHoraris();
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
