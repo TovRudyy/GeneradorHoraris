d3da433dfb8d25fbf110575b83bb7c2fe3f233fa
David Pujol <david.pujolperich@gmail.com>
2018-12-17 20:43:39 +0100

    Ja es poden afegir mes de un correquisit.

--- src/presentacio/VistaAfegirCorrequisit.java
+++ src/presentacio/VistaAfegirCorrequisit.java
@@ -55,22 +55,24 @@ public class VistaAfegirCorrequisit {
     }
 
     private void afegirCorrequisit() {
-        String assig = inputCorr.getText();
-        System.err.println("DEBUG: vols afegir el correquisit " + assig + " a " + this.assig);
-        if (VistaPrincipal.ctrl.existsAssignaturaPE(plaEstudi, assig)) {
-            if (!assig.equals(this.assig)) {
-                ArrayList<String> c = new ArrayList<String>();
-                c.add(assig);
-                VistaPrincipal.ctrl.afegirCorrequisit(plaEstudi, this.assig, c);
-                SeccioPlans.refrescaArbrePlansEstudis();
-                escenari.close();
-            }
-            else {
-                PopUpWindow.display("ERROR", "No es pot afegir de correquisit d'una assignatura a ella mateixa!");
-            }
-        }
-        else {
-            PopUpWindow.display("ERROR", "No existeix l'assignatura: "+assig);
-        }
+        String assig_aux = inputCorr.getText(); //suposem que hi poden haver diverses paraules
+        String assig[] = assig_aux.split(" ");
+
+        ArrayList<String> c = new ArrayList<>();
+       for (String a : assig) {
+           if (VistaPrincipal.ctrl.existsAssignaturaPE(plaEstudi, a)) {
+               System.err.println("DEBUG: vols afegir el correquisit " + a + " a " + this.assig);
+               if (!a.equals(this.assig)) {
+                   c.add(a);
+               } else
+                   PopUpWindow.display("ERROR", "No es pot afegir de correquisit d'una assignatura a ella mateixa!");
+           }
+           else
+               PopUpWindow.display("ERROR", "No existeix l'assignatura: "+assig);
+       }
+        VistaPrincipal.ctrl.afegirCorrequisit(plaEstudi, this.assig, c);
+        SeccioPlans.refrescaArbrePlansEstudis();
+        escenari.close();
+
     }
 }
