340099f25a88e82154221feef0ae5bc12046dd4d
DESKTOP-LSNRL66\victo <victordivi98@gmail.com>
2018-12-20 10:04:26 +0100

    Add javadoc

--- src/persistencia/ControladorPersistencia.java
+++ src/persistencia/ControladorPersistencia.java
@@ -13,31 +13,60 @@ import java.util.Objects;
 
 public class ControladorPersistencia {
 
+    /**
+     * @return retorna una llista de tots els objectes llegits dels arxius .json de la carpeta "data/PlaEstudis"
+     */
     public static ArrayList<Object> llegirCarpetaPlansJSON() {
         return Lector_Fitxers.llegirCarpetaPlansJSON();
     }
 
+    /**
+     * @return retorna una llista de tots els objectes llegits dels arxius .ser de la carpeta "data/PlaEstudis"
+     */
     public static ArrayList<Object> llegirCarpetaPlansSerialized() {
         return Lector_Fitxers.llegirCarpetaPlansSerialized();
     }
 
+    /**
+     * @param path ruta de l'arxiu .json a llegir
+     * @return objecte llegit de l'arxiu
+     * @throws IOException el fitxer és un directori, no es pot crear o no es pot obrir
+     * @throws ParseException el format de l'arxiu no és correcte
+     */
     public static Object llegirJSON(String path) throws IOException, ParseException {
         return Lector_Fitxers.llegirJSON(path);
     }
 
+    /**
+     * @return retorna una llista de tots els objectes llegits dels arxius .json de la carpeta "data/Aules"
+     */
     public static ArrayList<Object> llegirCarpetaAulesJSON() {
         return Lector_Fitxers.llegirCarpetaAulesJSON();
     }
 
+    /**
+     * @return retorna una llista de tots els objectes llegits dels arxius .ser de la carpeta "data/Aules"
+     */
     public static ArrayList<Object> llegirCarpetaAulesSerialized() {
         return Lector_Fitxers.llegirCarpetaAulesSerialized();
     }
 
 
+    /**
+     * @param path ruta del fitxer que conte l'objecte serialitzat
+     * @return objecte llegit deserialitzat
+     * @throws IOException el fitxer és un directori, no es pot crear o no es pot obrir
+     * @throws ClassNotFoundException la classe de lobjecte llegit és desconeguda
+     */
     public static Object carrega(String path) throws IOException, ClassNotFoundException {
         return Serialitzador.deserialize(path);
     }
 
+    /**
+     * @param object objecte que es vol serialitzar
+     * @param path ruta del fitxer on s'escriurà el fitxer serialitzat (es crearà o sobreescriurà si cal)
+     * @throws IOException el fitxer és un directori, no es pot crear o no es pot obrir
+     */
     public static void guarda(Object object, String path) throws IOException {
         Serialitzador.serialize(object, path);
     }
--- src/persistencia/Serialitzador.java
+++ src/persistencia/Serialitzador.java
@@ -2,11 +2,20 @@ package persistencia;
 
 import java.io.*;
 
+/**
+ * Classe per a serialitzar i deserialitzar objectes.
+ * No es pot instanciar, les dues funcions que té són estàtiques
+ */
 final class Serialitzador {
 
     private Serialitzador() {
     }
 
+    /**
+     * @param object objecte que es vol serialitzar
+     * @param path ruta del fitxer on s'escriurà el fitxer serialitzat (es crearà o sobreescriurà si cal)
+     * @throws IOException el fitxer és un directori, no es pot crear o no es pot obrir
+     */
     static void serialize(Object object, String path) throws IOException{
         if(!path.substring(path.lastIndexOf('.')+1).equals("ser")) path += ".ser";
         FileOutputStream fileOut = new FileOutputStream(path);
@@ -16,6 +25,12 @@ final class Serialitzador {
         fileOut.close();
     }
 
+    /**
+     * @param path ruta del fitxer que conte l'objecte serialitzat
+     * @return objecte llegit deserialitzat
+     * @throws IOException el fitxer és un directori, no es pot crear o no es pot obrir
+     * @throws ClassNotFoundException la classe de lobjecte llegit és desconeguda
+     */
     static Object deserialize(String path) throws IOException, ClassNotFoundException{
         FileInputStream fileIn = new FileInputStream(path);
         ObjectInputStream in = new ObjectInputStream(fileIn);
